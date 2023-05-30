ae770b01d1019b603cf1c77181ed0755b5fdd79c
Yilee <yileefrontend@gmail.com>
2023-05-30 23:14:09 +0800

    API路徑修正

--- src/utils/https.ts
+++ src/utils/https.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const https = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: `${import.meta.env.VITE_API_URL}/admin`
 })
 
 https.interceptors.request.use((config) => {
