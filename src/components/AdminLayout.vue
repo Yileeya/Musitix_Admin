18eaeb75ca71678f9961068156b40b4195591fe8
Yilee <yileefrontend@gmail.com>
2023-06-01 00:06:26 +0800

    [Layout]navbar、登出功能

--- src/components/AdminLayout.vue
+++ src/components/AdminLayout.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="admin-layout">
+    <navbar />
     <sidebar />
+    <RouterView />
   </div>
 </template>
 <script setup lang="ts">
 import Sidebar from '@/components/Sidebar.vue'
+import Navbar from '@/components/Navbar.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <style scoped lang="scss">
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <button class="btn btn-black" @click="logout">
+        <i class="fa fa-sign-out" />
+        登出
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const logout = async () => {
+  await localStorage.removeItem('Token')
+  await router.push('/')
+}
+</script>
+
+<style scoped></style>
