e21ddfd1a86a2aa58b21aa18a381592c64e5fe86
Yilee <yileefrontend@gmail.com>
2023-06-04 18:46:26 +0800

    增加日期組件

---
+++ src/components/ExtendedDatePicker.vue
@@ -0,0 +1,22 @@
+<template>
+  <date-picker v-bind="$attrs" :value="modelValue" :lang="langObject" @change="emitValue" />
+</template>
+
+<script setup>
+import 'vue-datepicker-next/locale/zh-tw'
+import DatePicker from 'vue-datepicker-next'
+import { ref } from 'vue'
+
+defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const langObject = ref({
+  formatLocale: {
+    firstDayOfWeek: 7
+  }
+})
+
+function emitValue(value) {
+  emit('update:modelValue', value)
+}
+</script>
