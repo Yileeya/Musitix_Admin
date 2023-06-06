11645c5ae866c381a463190f171084ef3ae4d74f
Yilee <yileefrontend@gmail.com>
2023-06-07 00:13:57 +0800

    移除ckeditor相關程式

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'bootstrap'
-import CKEditor from '@ckeditor/ckeditor5-vue'
 import { toast, options } from './plugins/toastification'
 
 import App from './App.vue'
@@ -11,4 +10,4 @@ import './assets/main.scss'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(router).use(CKEditor).use(toast, options).mount('#app')
+app.use(createPinia()).use(router).use(toast, options).mount('#app')
