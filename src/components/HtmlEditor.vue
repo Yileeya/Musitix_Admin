c27fc1979d8c63defdf8db87833a7c111c522206
Yilee <yileefrontend@gmail.com>
2023-06-06 20:55:27 +0800

    ckeditor套件設定

--- src/components/HtmlEditor.vue
+++ src/components/HtmlEditor.vue
@@ -1,16 +1,20 @@
 <template>
-  <ckeditor :editor="editor" v-model="editorData" />
+  <ckeditor :editor="editor" :config="config" v-model="editorData" />
 </template>
 
 <script setup>
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { ref, watch } from 'vue'
+import Editor from 'ckeditor5-custom-build/build/ckeditor'
 
 const props = defineProps(['modelValue'])
 const emit = defineEmits(['update:modelValue'])
 
-const editor = ref(ClassicEditor)
-const editorData = ref(props.modelValue ? props.modelValue : '<p>請輸入內容</p>')
+const editor = ref(Editor)
+const editorData = ref(props.modelValue ? props.modelValue : '')
+const config = ref({
+  placeholder: '請輸入內容',
+  removePlugins: ['Title', 'ImageInsert', 'ImageUpload', 'SimpleUploadAdapter']
+})
 
 watch(
   () => props.modelValue,
