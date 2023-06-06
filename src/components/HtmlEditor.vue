339c2d7db39d8844268c7ec6bb5457288b5c3ea6
Yilee <yileefrontend@gmail.com>
2023-06-06 23:47:08 +0800

    ckeditor改以vite模式

--- src/components/HtmlEditor.vue
+++ src/components/HtmlEditor.vue
@@ -1,18 +1,42 @@
 <template>
-    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"/>
+    <div>
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+    </div>
 </template>
 
 <script>
-import * as Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+
 export default {
     data() {
         return {
-            editor: Editor,
+            editor: ClassicEditor,
             editorData: '<p>Content of the editor.</p>',
             editorConfig: {
-                // The configuration of the editor.
+                plugins: [
+                    Essentials,
+                    Bold,
+                    Italic,
+                    Paragraph,
+                    SourceEditing
+                ],
+
+                toolbar: {
+                    items: [
+                        'bold',
+                        'italic',
+                        'undo',
+                        'redo',
+                        'sourceEditing'
+                    ]
+                }
             }
         };
     }
-}
+};
 </script>
