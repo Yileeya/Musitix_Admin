65848fe88332c700de18ba8689df5d56d3fcce5d
Yilee <yileefrontend@gmail.com>
2023-06-13 22:25:51 +0800

    [活動新增]增加datepicker disabled條件

--- src/views/Activity/ActivityLayout.vue
+++ src/views/Activity/ActivityLayout.vue
@@ -5,6 +5,7 @@
     <content v-show="currentTab === 'content'" />
     <schedule-tickets v-show="currentTab === 'scheduleTickets'" />
   </div>
+  <button class="btn btn-primary btn-lg fix-button" @click="saveActivity">儲存</button>
 </template>
 
 <script setup lang="ts">
@@ -13,12 +14,27 @@ import TabsTitle from '@/views/Activity/TabsTitle.vue'
 import Information from '@/views/Activity/Information.vue'
 import Content from '@/views/Activity/Content.vue'
 import ScheduleTickets from '@/views/Activity/ScheduleTickets.vue'
+import { activityHandle } from '@/stores/activityHandle'
 
 const currentTab = ref<string>('information')
+
+const activityData = activityHandle()
+const saveActivity = () => {
+  let submitForm = { ...activityData.formatResult }
+  submitForm.HtmlContent = activityData.HtmlContent
+  submitForm.HtmlNotice = activityData.HtmlNotice
+  submitForm.schedules = [...activityData.schedules]
+  console.log(submitForm)
+}
 </script>
 
 <style scoped lang="scss">
 .activity-layout {
   padding: 30px;
+}
+.fix-button {
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
 }
 </style>
--- src/views/Activity/ScheduleTickets.vue
+++ src/views/Activity/ScheduleTickets.vue
@@ -31,11 +31,14 @@
                       ? [schedule.startTime, schedule.endTime]
                       : [schedule.saleStartTime, schedule.saleEndTime]
                   "
+                  :disabled-date="
+                    (date:any) => disabledDate(date, index === 1 ? 'scheduleDate' : 'scheduleSaleDate')
+                  "
                   class="date-picker"
                   range
                   type="datetime"
+                  :disabled="!activityHandle().information.dateRange.length"
                   :placeholder="'請選擇' + (index === 1 ? '起訖' : '販售') + '區間'"
-                  :disabled-start-date="index === 2 ? schedule.startTime : null"
                   @update:modelValue="(value) => updateDateValue(value, index, scheduleIndex)"
                 />
               </div>
@@ -110,6 +113,7 @@
 import ExtendedDatePicker from '@/components/ExtendedDatePicker.vue'
 import { activityHandle } from '@/stores/activityHandle'
 import _ from 'lodash'
+
 const schedules = activityHandle().schedules
 
 // 判斷是否都有值
@@ -126,6 +130,17 @@ const updateDateValue = (value: Array<string>, index: number, scheduleIndex: num
     //銷售
     schedules[scheduleIndex].saleStartTime = value.length ? value[0] : null
     schedules[scheduleIndex].saleEndTime = value.length ? value[1] : null
+  }
+}
+
+function disabledDate(date: any, target: string) {
+  const activityDate = activityHandle().information?.dateRange
+  const startDate = new Date(activityDate[0]).setHours(0, 0, 0, 0)
+  if (target === 'scheduleDate') {
+    const endDate = new Date(activityDate[1]).setHours(0, 0, 0, 0)
+    return date < startDate || date > endDate
+  } else {
+    return date > startDate
   }
 }
 
@@ -154,47 +169,59 @@ const removeSchedule = (scheduleIndex: number) => {
 <style scoped lang="scss">
 .schedule-tickets {
   padding: 30px 0;
+
   .add-schedule-btn {
     float: right;
     margin-bottom: 10px;
     margin-top: -30px;
   }
+
   table {
     width: 100%;
     margin-bottom: 50px;
+
     td,
     th {
       padding: 15px;
     }
+
     thead {
       background-color: #989898;
       color: white;
+
       .form-group {
         display: flex;
         gap: 15px;
         align-items: baseline;
+
         .form-control {
           flex: 1;
         }
       }
+
       .date-picker-group {
         display: flex;
         gap: 30px;
         margin-top: 20px;
+
         .date-picker {
           width: 370px;
         }
       }
     }
+
     tbody {
       background-color: #e4e4e4;
+
       .title-tr {
         font-weight: bold;
         text-align: center;
+
         td {
           padding: 10px 0 0;
         }
       }
+
       .btn-sm {
         margin: 0 5px;
       }
