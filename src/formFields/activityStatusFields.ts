a66e9ba97f9cf0da3cfe7d388a3fc7c0ff3747b0
Yilee <yileefrontend@gmail.com>
2023-06-14 23:27:15 +0800

    [活動管理]activityStatus列表

---
+++ src/formFields/activityStatusFields.ts
@@ -0,0 +1,28 @@
+const activityStatusFields = [
+  {
+    id: 1,
+    name: 'Unpublished',
+    title: '未上架'
+  },
+  {
+    id: 2,
+    name: 'Published',
+    title: '已上架'
+  },
+  {
+    id: 3,
+    name: 'Ended',
+    title: '已結束'
+  },
+  {
+    id: 4,
+    name: 'Canceled',
+    title: '已取消'
+  },
+  {
+    id: 5,
+    name: 'Discontinued',
+    title: '已停辦'
+  }
+]
+export default activityStatusFields
--- src/views/Activities/StatusSelect.vue
+++ src/views/Activities/StatusSelect.vue
@@ -1,17 +1,16 @@
 <template>
   <select class="form-select" :value="status" @change="$emit('update:status', $event.target.value)">
     <option :value="''">狀態</option>
-    <option v-for="status in statusOptions" :value="status">{{ status }}</option>
+    <option v-for="status in activityStatusFields" :value="status.id">{{ status.title }}</option>
   </select>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import activityStatusFields from '@/formFields/activityStatusFields'
 
 defineProps({
   status: String
 })
 
 defineEmits(['update:status'])
-const statusOptions = ref(['已上架', '未上架', '已結束', '已取消', '已停辦'])
 </script>
